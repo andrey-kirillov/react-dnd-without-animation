56e65b918825b180d19d589cfb6a780fbbedb0f4
andrey_kir <andrey.kirillov.kiev@gmail.com>
2020-01-25 05:09:31 +0200

    simple dnd without animation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let currentItem = null;
+    const [arr, setArr] = useState([
+        {id: 0, name: 'first'},
+        {id: 1, name: 'second'},
+        {id: 2, name: 'third'},
+        {id: 3, name: 'forth'}
+    ]);
+
+    const allowDrop = e => {
+        e.preventDefault();
+    };
+
+    const drop = e => {
+        e.preventDefault();
+        if (+e.target.dataset.id !== +currentItem.id) {
+            const newArr = arr.slice();
+            newArr.splice(arr.findIndex(item => item.id === currentItem.id), 1);
+            newArr.splice(+e.target.dataset.index, 0, currentItem);
+            setArr(newArr);
+        }
+    };
+
+    return (
+        <div className="App" onDragOver={allowDrop} onDrop={drop}>
+
+            {
+                arr.map((item, i) => {
+                    return (
+                        <div className={"item"}
+                             key={item.id}
+                             draggable
+                             data-index={i}
+                             onMouseDown={() => currentItem = item}
+                        >
+                            {item.name}
+                        </div>
+                    )
+                })
+            }
+
+        </div>
+    );
 }
 
 export default App;
